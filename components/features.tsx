2b19da43907edf6d2190f44f0a7b0d45bd53edc9
Robinson PG <Robinson.netdevelop@gmail.com>
2023-05-13 19:37:38 -0500

    Primera publicacion del sitio Americacom

--- components/features.tsx
+++ components/features.tsx
@@ -6,8 +6,8 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 className="h2 mb-4">Servicios de instalación de antenas de telecomunicaciones.</h2>
+            <p className="text-xl text-gray-400">Somos una empresa joven motivados por realizar un servicio confiable, rápido de bajos costos.</p>
           </div>
 
           {/* Items */}
